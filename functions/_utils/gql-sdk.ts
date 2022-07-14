ba78f1cdf9e32df99943d73fbb4d36c92cbce053
Johan Eliasson <johan@eliasson.me>
2022-07-14 21:41:28 +0200

    ts fix

--- functions/_utils/gql-sdk.ts
+++ functions/_utils/gql-sdk.ts
@@ -3,7 +3,7 @@ import { getSdk } from './__generated__/graphql-request'
 
 const client = new GraphQLClient(`${process.env.NHOST_BACKEND_URL}/v1/graphql`, {
   headers: {
-    'x-hasura-admin-secret': process.env.NHOST_ADMIN_SECRET
+    'x-hasura-admin-secret': process.env.NHOST_ADMIN_SECRET as string
   }
 })
 export const gqlSDK = getSdk(client)
